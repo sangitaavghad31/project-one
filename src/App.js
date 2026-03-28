56541b1b68032eb2452a29b46994d60709a4e8e5
sangita avghad <sangitaavghad1987@gmail.com>
2026-03-28 15:46:26 +0530

    Sangita: Creates Expense Item components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div className="App">
       <h1>Let's get starts</h1>
       <h1>Hello World</h1>
       <p>I am learning react</p>
+      <ExpenseItem/>
       <p/>
     </div>
   );
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+import "../index.css";
+export default function ExpenseItem(){
+    return <div>
+        <p>This is expense item</p>
+        <ul>
+            <li>Food - $5</li>
+            <li>Petrol - $20</li>
+            <li>Movie - $10</li>
+        </ul>
+    </div>
+}
