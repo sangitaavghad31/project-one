d01e62ea4ca4235db7da416e3aa583b3ae16fe48
sangita avghad <sangitaavghad1987@gmail.com>
2026-03-29 13:50:50 +0530

    Sangita: using map, iterated expense item object

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
-// Write your code at relevant places in the code below:
+// Write your code at the relevant places in the code below:
 
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { id: "1", location:"Bangalore", date: new Date(2023, 7, 15), title: "Insurance", price: 100 },
+    { id: "2", location:"Delhi", date: new Date(2023, 3, 25), title: "Book", price: 10 },
+    { id: "3", location:"Hyderabad", date: new Date(2023, 10, 11), title: "Pen", price: 1 },
+    { id: "4", location:"Mumbai", date: new Date(2023, 1, 14), title: "Laptop", price: 200 },
+  ];
+
   return (
     <div>
       <h1>Let's get Started</h1>
-      <ExpenseItem
-        date={new Date(2023, 7, 15)}
-        location="Bangalore"
-        title="Insurance"
-        price="50"
-      ></ExpenseItem>
-      <ExpenseItem
-        date={new Date(2023, 3, 25)}
-        location="Delhi"
-        title="Book"
-        price="20"
-      ></ExpenseItem>
-      <ExpenseItem
-        date={new Date(2023, 10, 11)}
-        location="Hyderabad"
-        title="Pen"
-        price="5"
-      ></ExpenseItem>
-      <ExpenseItem
-        date={new Date(2023, 1, 14)}
-        location="Mumbai"
-        title="Laptop"
-        price="200"
-      ></ExpenseItem>
+      {expenses.map((expense, index) => {
+        return (
+          <ExpenseItem
+            key={expense.id}
+            location={expense.location}
+            date={expense.date}
+            title={expense.title}
+            price={expense.price}
+          ></ExpenseItem>
+        );
+      })}
     </div>
   );
 }
 
 export default App;
+ç
