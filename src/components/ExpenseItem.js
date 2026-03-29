f7e6c1d58c1713e96f495a77ff2d6e5cdd8bf7b9
sangita avghad <sangitaavghad1987@gmail.com>
2026-03-30 00:23:19 +0530

    Sangita: Adds change title event handler

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,18 @@
-import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  function changeTitleOnclickHandler(event) {
+    console.log(`Button clicked`);
+  }
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}></ExpenseDate>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
       </div>
+      <button onClick={changeTitleOnclickHandler}>Change Title</button>
     </Card>
   );
 }
