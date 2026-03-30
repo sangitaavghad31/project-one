c4a2e25a52c1421e975e0aa862082d498f637755
sangita avghad <sangitaavghad1987@gmail.com>
2026-03-30 13:52:11 +0530

    Sangita: refactor folder structure and add hander for date price and title

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [price, setPrice] = useState(null);
+  const [date, setDate] = useState();
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const priceChangeHandler = (event) => {
+    setPrice(event.target.value);
+  }
+
+  const dateChangeHandler = (event) => {
+    setDate(event.target.value);
+  }
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label htmlFor="amount">Amount</label>
+          <input type="number" id="amount" onChange={priceChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label htmlFor="date">Date</label>
+          <input type="date" id="date" min="2023-01-01" max="2024-12-31" onChange={dateChangeHandler} />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
