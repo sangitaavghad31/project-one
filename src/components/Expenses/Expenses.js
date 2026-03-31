b829a23bce0500a3bce549151e889ead31035473
sangita avghad <sangitaavghad1987@gmail.com>
2026-04-01 00:10:16 +0530

    Sangita: creates separate component to generate child component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 // Write your code at relevant places in the code below:
 
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 import Card from "../UI/Card";
 
@@ -17,32 +17,13 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found</p>;
-  if(filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => {
-
-      return (
-        <>
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          date={expense.date}
-          price={expense.price}
-          />
-          {filteredExpenses.length==1 && (<p>Only one expense found. Please add more</p>)}
-        </>
-      );
-    });
-    
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={changeFilterHandler}
       />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
